7c3ee75173fc31317e913cd4633a14ec007fe8b2
RajeshVR999 <105232983+RajeshVR999@users.noreply.github.com>
2024-08-06 00:24:38 +0530

    updated

--- 02-vars/sample.tf
+++ 02-vars/sample.tf
@@ -18,6 +18,7 @@ output "number" {
     value = var.number # printing the variables
 }
 
+# List Variable
 variable "ex-list" {
     default = [
         "cloud",
@@ -33,6 +34,21 @@ output "ex-list-output" {
     value = "welcome to ${var.ex-list[0]} ${var.ex-list[1]} the terraform duration is ${var.ex-list[2]} terform expert name is ${var.ex-list[4]}"
 }
 
+# Map Variable 
+
+variable "ex-map" {
+    default = {
+        Duration  = 105 
+        Timing    = "0730AMIST"
+        Batch     = 51 
+        teraform  = "Rajesh VR" 
+        
+    }
+}
+
+output "ex-map" {
+    value = "Wlcome to batch ${var.ex-map["Batch"]} and the sessions timings are ${var.ex-map["Timing"]} and training name is ${var.ex-map["Trainer"]}"
+}
 
 
 # A variable can be accessed without ${} only the variable is called. In case, if it's needed to be added with other strings or in between something, then we need to enclose them in

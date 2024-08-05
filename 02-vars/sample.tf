3b6f98ba3d8eaeddec268472e5cfb5c09ce8f389
RajeshVR999 <105232983+RajeshVR999@users.noreply.github.com>
2024-08-05 23:51:58 +0530

    updated

--- 02-vars/sample.tf
+++ 02-vars/sample.tf
@@ -18,6 +18,17 @@ output "number" {
     value = var.number # printing the variables
 }
 
+variable "ex-list" {
+    default = [
+        "cloud"
+        "Devops"
+        100
+        false
+        "verma"
+        "SRE"
+    ]
+}
+
 
 # A variable can be accessed without ${} only the variable is called. In case, if it's needed to be added with other strings or in between something, then we need to enclose them in
 # in the double quotes. Single quotes are not valid in terraform. 

4d3fca6e49ba2314d54b7811ef8b943bbb37b2a4
RajeshVR999 <105232983+RajeshVR999@users.noreply.github.com>
2024-08-06 14:12:56 +0530

    updated

--- example-infra/ec2.tf
+++ example-infra/ec2.tf
@@ -1,6 +1,6 @@
-resource "aws_instance" "ec2-web" {
+resource "aws_instance" "app" {
   ami = "ami-0d159bae10a240227"
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
   
   tags = {
     Name = "CAE-CERT-AWS_EC2"

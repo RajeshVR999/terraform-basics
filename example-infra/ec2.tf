bf0979fe8ac2b0c8646dd6469b8b7fcfdc973e30
RajeshVR999 <105232983+RajeshVR999@users.noreply.github.com>
2024-08-06 15:55:36 +0530

    updated

--- example-infra/ec2.tf
+++ example-infra/ec2.tf
@@ -27,6 +27,22 @@ resource "aws_security_group" "allow_ssh" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+      description      = Allow HTTP From Public
+      from_port        = 80
+      to_port          = 80
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+    }
+
+    ingress {
+      description      = Allow HTTPS From Public
+      from_port        = 443
+      to_port          = 443
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+    }
+
   egress {
     from_port = 0
     to_port = 0

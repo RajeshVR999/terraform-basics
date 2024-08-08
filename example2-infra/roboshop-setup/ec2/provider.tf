b2246109cc56ee57431bffc5d144ef7082b79cbe
RajeshVR999 <105232983+RajeshVR999@users.noreply.github.com>
2024-08-08 23:20:21 +0530

    updated

--- example2-infra/roboshop-setup/ec2/provider.tf
+++ example2-infra/roboshop-setup/ec2/provider.tf
@@ -1,12 +1,1 @@
-terraform {
-  backend "s3" {
-    bucket = "krishya-tf-statefile"  # Ensure this bucket name is valid and exists
-    key    = "example2/terraform.tfstate"
-    region = "ap-southeast-1"  # Specify the correct region for your S3 bucket
-  }
-}
-
-
-provider "aws" {
-  region = "ap-southeast-1"
-} 
+#####

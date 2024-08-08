2f4e8977aa8768babaf72cee83129e52782a91ae
RajeshVR999 <105232983+RajeshVR999@users.noreply.github.com>
2024-08-08 12:31:09 +0530

    s3

--- example-infra/backend.tf
+++ example-infra/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "s3://krishya-tf-statefile/"  # Ensure this bucket name is valid and exists
+    bucket = "krishya-tf-statefile"  # Ensure this bucket name is valid and exists
     key    = "sample/terraform.tfstate"
     region = "ap-southeast-1"  # Specify the correct region for your S3 bucket
   }
@@ -14,3 +14,5 @@ terraform {
 #     region = "ap-southeast-1"
 #   }
 # }
+
+//https://krishya-tf-statefile.s3.ap-southeast-1.amazonaws.com/sample/

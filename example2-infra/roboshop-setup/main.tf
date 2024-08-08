4a9b90c7718185458fe80135c7c26c63f9c62137
RajeshVR999 <105232983+RajeshVR999@users.noreply.github.com>
2024-08-08 23:48:29 +0530

    updated

--- example2-infra/roboshop-setup/main.tf
+++ example2-infra/roboshop-setup/main.tf
@@ -15,4 +15,59 @@ module "frontend" {
     COMPONET = "frontend"
 }
 
+module "mongodb" {
+    source = "./ec2"
+    COMPONET = "mongodb"
+}
+
+module "catalogue" {
+    source = "./ec2"
+    COMPONET = "catalogue"
+}
+
+module "redis" {
+    source = "./ec2"
+    COMPONET = "redis"
+}
+
+module "user" {
+    source = "./ec2"
+    COMPONET = "user"
+}
+
+module "cart" {
+    source = "./ec2"
+    COMPONET = "cart"
+}
+
+module "mysql" {
+    source = "./ec2"
+    COMPONET = "mysql"
+}
+
+module "shipping" {
+    source = "./ec2"
+    COMPONET = "shipping"
+}
+
+module "rabnitmq" {
+    source = "./ec2"
+    COMPONET = "rabbitmq"
+}
+
+module "payment" {
+    source = "./ec2"
+    COMPONET = "payment"
+}
+
+module "dispatch" {
+    source = "./ec2"
+    COMPONET = "dispatch"
+}
+
+
+
+
+
+
 

285342a914acee9280ea0bb99c601f5d68e0f4bb
RajeshVR999 <105232983+RajeshVR999@users.noreply.github.com>
2024-08-08 19:29:56 +0530

    s3

--- example2-infra/ec2/main.tf
+++ example2-infra/ec2/main.tf
@@ -2,7 +2,7 @@
 resource "aws_instance" "app" {
   ami                    = "ami-0d159bae10a240227"
   instance_type          = "t3.micro"
-  vpc_security_group_ids = var.sg
+  vpc_security_group_ids = [var.sg]
   tags = {
     Name = "CAE-CERT-AWS_EC2"
   }
@@ -12,6 +12,4 @@ resource "aws_instance" "app" {
 
 variable "sg" {}
 
-output "public_ip" {
-  value = aws_instance.app.public_ip
-  }
+
--- example2-infra/main.tf
+++ example2-infra/main.tf
@@ -9,6 +9,3 @@ module "sg" {
 
 # Taking the output to this root module from the sg sub-module
 
-output "public_ip" {
-  value = module.ec2.public_ip
-  }

69ce971b6c2e8af2d84922dbf7207f15da490465
RajeshVR999 <105232983+RajeshVR999@users.noreply.github.com>
2024-08-08 23:34:15 +0530

    updated

--- example2-infra/roboshop-setup/ec2/ec2.tf
+++ example2-infra/roboshop-setup/ec2/ec2.tf
@@ -1,7 +1,7 @@
 # Request a spot instance at $0.03
 
 resource "aws_spot_instance_request" "spot" {
-  ami = "data.aws_ami.myami.id"
+  ami = data.aws_ami.myami.id
   instance_type = "t3.micro"
   wait_for_fulfillment = true
   vpc_security_group_ids = [aws_security_group.allows_ssh.id]

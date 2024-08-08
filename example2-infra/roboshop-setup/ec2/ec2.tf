a518eaf01ee2c38ed80b8ea12063189c9dde0455
RajeshVR999 <105232983+RajeshVR999@users.noreply.github.com>
2024-08-08 23:23:51 +0530

    updated

--- example2-infra/roboshop-setup/ec2/ec2.tf
+++ example2-infra/roboshop-setup/ec2/ec2.tf
@@ -4,7 +4,7 @@ resource "aws_spot_instance_request" "spot" {
   ami = "data.aws_ami.myami.id"
   instance_type = "t3.micro"
   wait_for_fulfillment = true
-  vpc_security_group_ids = [aws_security_group.allows_ssh]
+  vpc_security_group_ids = [aws_security_group.allows_ssh.id]
 
   tags = {
     Name = var.COMPONET

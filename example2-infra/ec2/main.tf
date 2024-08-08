1b9e17784fc903c0b00fa0a384a36fdc33dccb4b
RajeshVR999 <105232983+RajeshVR999@users.noreply.github.com>
2024-08-08 21:14:07 +0530

    s3

--- example2-infra/ec2/main.tf
+++ example2-infra/ec2/main.tf
@@ -3,15 +3,25 @@ resource "aws_instance" "app" {
   ami                    = data.aws_ami.myami.image_id
   instance_type          = "t3.micro"
   vpc_security_group_ids = [var.sg]
+
+  connection {
+    type = "ssh"
+    user = "ec2-user"
+    password = "Devops@999"
+    host = self.private_ip
+  }
+
+  provisioner "remote-exec" {
+    
+  }
   tags = {
     Name = "CAE-CERT-AWS_EC2"
   }
-  provisioner "local-exec" {
-    command = <<EOF
-    cd /home/ec2-user/ansible
-    ansible -i self.private_ip -e ENV=dev COMPONENT=mongodb roboshop.yml
-    EOF
-}
+#   provisioner "local-exec" {
+#     command = <<EOF
+#     cd /home/ec2-user/ansible
+#     ansible -i self.private_ip -e ENV=dev COMPONENT=mongodb roboshop.yml
+    
 }
 
 

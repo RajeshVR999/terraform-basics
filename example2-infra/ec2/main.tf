a7382a1fd55513bd6ad6b4335b1de5271b845b09
RajeshVR999 <105232983+RajeshVR999@users.noreply.github.com>
2024-08-08 15:59:06 +0530

    s3

--- example2-infra/ec2/main.tf
+++ example2-infra/ec2/main.tf
@@ -2,7 +2,7 @@
 resource "aws_instance" "app" {
   ami                    = "ami-0d159bae10a240227"
   instance_type          = "t3.micro"
-  vpc_security_group_ids = var.sg
+  vpc_security_group_ids = "var.sg"
   tags = {
     Name = "CAE-CERT-AWS_EC2"
   }

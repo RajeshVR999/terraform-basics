3b7efaf01d0d3a860fcceda4467aefdf299bad3a
RajeshVR999 <105232983+RajeshVR999@users.noreply.github.com>
2024-08-08 15:52:06 +0530

    s3

--- example2-infra/ec2/main.tf
+++ example2-infra/ec2/main.tf
@@ -2,7 +2,7 @@
 resource "aws_instance" "app" {
   ami                    = "ami-0d159bae10a240227"
   instance_type          = "t3.micro"
-  #vpc_security_group_ids = [var.sg]
+  vpc_security_group_ids = var.sg
   tags = {
     Name = "CAE-CERT-AWS_EC2"
   }
@@ -10,10 +10,10 @@ resource "aws_instance" "app" {
 
 
 
-# variable "sg" {
-#   description = "Security group ID for the EC2 instance"
-#   type        = 
-# }
+variable "sg" {
+  description = "Security group ID for the EC2 instance"
+  type        = 
+}
 
 output "public_ip" {
   value = aws_instance.app.public_ip
